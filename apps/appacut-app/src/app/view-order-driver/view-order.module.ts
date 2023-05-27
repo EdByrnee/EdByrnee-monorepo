c05ece09160d4db4f1587daa4e0b7ab70729ea22
Ed Byrne <ed.byrne@ardanis.com>
2023-05-27 22:16:39 +0100

    save changes

--- apps/appacut-app/src/app/view-order-driver/view-order.module.ts
+++ apps/appacut-app/src/app/view-order-driver/view-order.module.ts
@@ -8,9 +8,10 @@ import { ViewOrderPageRoutingModule } from './view-order-routing.module';
 
 import { ViewOrderPage } from './view-order.page';
 import { OrderTrackingComponent } from '../order-tracking/order-tracking.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, ViewOrderPageRoutingModule],
+  imports: [CommonModule, FormsModule, IonicModule, ViewOrderPageRoutingModule, SharedModule],
   declarations: [ViewOrderPage, OrderTrackingComponent],
 })
 export class ViewOrderPageModule {}
